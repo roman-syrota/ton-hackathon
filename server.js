8ba1ef27b1eddae965fd9ee63f26c858ec7d4199
romansirota <roma@mailroma.com>
2024-10-13 10:41:08 +0300

    init commit

---
+++ server.js
@@ -0,0 +1,136 @@
+import express from 'express';
+import fs from 'fs';
+import session from 'express-session';
+import dotenv from 'dotenv';
+import { body, validationResult } from 'express-validator'; 
+import TonWeb from "tonweb";
+//import { WalletV4ContractR2 } from 'tonweb/dist/types/contract/wallet/v4/wallet-v4-contract-r2';
+
+dotenv.config();
+
+
+const BN = TonWeb.utils.BN;
+const NODE_API_URL = 'https://toncenter.com/api/v2/jsonRPC';
+//const INDEX_API_URL = 'https://toncenter.com/api/index/';
+const tonweb = new TonWeb(new TonWeb.HttpProvider(NODE_API_URL, {apiKey: process.env.TONCENTER_API_KEY}));
+const masterKeyPair = TonWeb.utils.nacl.sign.keyPair.fromSecretKey(TonWeb.utils.hexToBytes(process.env.JETTON_MASTER_KEY));
+
+const getJettonWallet = () => {
+    //const keyPair = nacl.box.keyPair.fromSecretKey(process.env.TOKEN_MASTER_KEY);
+    const WalletClass = tonweb.wallet.all['v4R2'];
+    const wallet = new WalletClass(tonweb.provider, {
+        publicKey:  masterKeyPair.publicKey,
+        wc: 0
+    });
+    //const wallet = tonweb.wallet.create({publicKey: masterKeyPair.publicKey, WalletV4ContractR2});
+    return wallet;
+}
+const getWallet = (address) => {
+    const wallet = tonweb.wallet.create({address: address});
+    return wallet;
+} 
+
+const sendJettons = async (winnerAddress) => {
+    const jettonWalletAddress =  process.env.JETTON_WALLET_ADDRESS;
+    const jettonWallet = new TonWeb.token.jetton.JettonWallet(tonweb.provider, {address: jettonWalletAddress});
+    const wallet = getTokenWallet();
+    const seqno = await wallet.methods.seqno().call() || 0
+
+    const transfer = await wallet.methods.transfer({
+        secretKey: masterKeyPair.secretKey,
+        toAddress: jettonWalletAddress,
+        amount: 0,
+        seqno: seqno,
+        //sendMode: 128,
+        payload: await jettonWallet.createTransferBody({
+            queryId: seqno, 
+            jettonAmount: process.env.REWARD_AMOUNT, // jetton amount in units
+            toAddress: new TonWeb.utils.Address(winnerAddress),
+            responseAddress: new TonWeb.utils.Address(winnerAddress)
+        })
+    });
+
+    await transfer.send();
+}
+
+
+
+const app = express();
+const port = 3333;
+
+// Use built-in JSON parser
+app.use(express.json());
+
+// Session configuration
+app.use(session({
+  secret: process.env.SESSION_SECRET, 
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false } // Set to true if using HTTPS
+}));
+
+// GET: /question/get
+app.get('/question/get', async (req, res) => {
+  try {
+    const data = await fs.promises.readFile('questions.json', 'utf8');
+    const questions = JSON.parse(data);
+    res.json(questions);
+  } catch (err) {
+    console.error('Error processing request:', err);
+    res.status(500).send('Server error');
+  }
+});
+
+// POST: /question/answer
+app.post('/question/answer',
+    // Validation middleware
+    body('answers').isArray().withMessage('answers must be an array'),
+    body('answers.*').isObject().withMessage('Each item in answers must be an object'),
+    body('answers.*.question').isString().withMessage('question must be a string'),
+    body('answers.*.answer').isString().withMessage('answer must be a string'),
+    async (req, res) => {
+      // Handle validation errors
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        console.error('Validation errors:', errors.array());
+        return res.status(400).json({ errors: errors.array() });
+      }
+  
+      const { answers } = req.body; 
+  
+
+  
+      console.log('Received answers:', answers);
+  
+      res.send('Answers received');
+    }
+  );
+  
+
+// GET: /session/start
+app.get('/session/start', (req, res) => {
+  const wallet = req.query.wallet || null;
+  // Save wallet in session if provided
+  if (wallet) {
+    req.session.wallet = wallet;
+  }
+  // Initialize session if this is the first request
+  if (!req.session.userId) {
+    req.session.userId = generateUniqueId();
+  }
+ 
+  res.json({ session: req.session.userId });
+});
+
+// Function to generate a unique user identifier
+function generateUniqueId() {
+  return Math.random().toString(36).substr(2, 9);
+}
+
+
+// app.listen(port, () => {
+//   console.log(`Server is running at http://localhost:${port}`);
+// });
+const wallet = getJettonWallet();
+const walletAddress = await wallet.getAddress();
+console.log(walletAddress.toString(true, true, true));
---
+++ wallet.js
@@ -0,0 +1,37 @@
+//we need to crearte wallet to hold jettons for rewards
+import TonWeb from "tonweb";
+import TonWebMnemonic from "tonweb-mnemonic";
+import fs from "fs";
+import dotenv from 'dotenv';
+import * as bip39 from 'bip39';
+import * as ed25519 from 'ed25519-hd-key';
+import { exit } from "process";
+
+dotenv.config();
+
+const NODE_API_URL = 'https://toncenter.com/api/v2/jsonRPC';
+const tonweb = new TonWeb(new TonWeb.HttpProvider(NODE_API_URL, {apiKey: process.env.TONCENTER_API_KEY}));
+
+    // Generate a 12-word mnemonic
+//const mnemonic = bip39.generateMnemonic(128); // 128 bits of entropy
+const mnemonic = 'clap shallow twist onion torch track warfare pond level fringe cute angry';
+const keyPair = TonWeb.utils.keyPairFromSeed(await TonWebMnemonic.mnemonicToSeed(mnemonic.split(' ')))
+
+const WalletClass = tonweb.wallet.all['v4R2'];
+const wallet = new WalletClass(tonweb.provider, {
+    publicKey: keyPair.publicKey,
+    wc: 0
+});
+
+const walletAddress = await wallet.getAddress();
+const addressString = walletAddress.toString(true, true, false);
+
+console.log('Jettons wallet:', addressString);
+console.log('Send jettons for rewards and TON for gas to this address');
+const keyPairData = {
+    publicKey: TonWeb.utils.bytesToHex(keyPair.publicKey),
+    secretKey: TonWeb.utils.bytesToHex(keyPair.secretKey),
+    mnemonic: mnemonic
+};
+fs.writeFileSync('jetton_keeper_wallet.json', JSON.stringify(keyPairData, null, 2));
+
